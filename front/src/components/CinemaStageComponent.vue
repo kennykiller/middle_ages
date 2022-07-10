fe646a126b9d646d7024e16731aab74a1690b86f
kennykiller <vadim2505@mail.ru>
2022-07-10 12:05:14 +0300

    #32 cinema stage

--- front/src/components/CinemaStageComponent.vue
+++ front/src/components/CinemaStageComponent.vue
@@ -1,49 +1,102 @@
 <script setup lang="ts">
-const arr = [
-  { isBusy: true, id: 1 },
-  { isBusy: false, id: 2 },
-  { isBusy: true, id: 3 },
-  { isBusy: false, id: 4 },
-];
+const arr = [];
+const rows = [...Array(11).keys()].slice(1);
+console.log(rows);
+
+let id = 1;
+while (arr.length < 100) {
+  arr.push({ isBusy: false, id: id++ }, { isBusy: true, id: id++ });
+}
 </script>
 
 <template>
   <div class="stage">
-    <ul class="stage__list">
-      <li class="stage__list-item seat" v-for="seat of arr" :key="seat.id">
-        <img
-          class="seat__image seat__image--busy"
-          v-if="seat.isBusy"
-          src="@/assets/images/busy-seat.png"
-          alt="busy"
-        />
-        <img
-          class="seat__image seat__image--free"
-          v-if="!seat.isBusy"
-          src="@/assets/images/free-seat.png"
-          alt="free"
-        />
-      </li>
-    </ul>
+    <div class="stage__screen"><span>Экран</span></div>
+    <div class="stage__scheme scheme">
+      <ul class="scheme__rows">
+        <li class="scheme__row" v-for="row of rows" :key="row">
+          <span>{{ row }}</span>
+        </li>
+      </ul>
+      <ul class="stage__list">
+        <li class="stage__list-item seat" v-for="seat of arr" :key="seat.id">
+          <img
+            class="seat__image seat__image--busy"
+            v-if="seat.isBusy"
+            src="@/assets/images/busy-seat.png"
+            alt="busy"
+          />
+          <img
+            class="seat__image seat__image--free"
+            v-if="!seat.isBusy"
+            src="@/assets/images/free-seat.png"
+            alt="free"
+          />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .stage {
   width: 640px;
-  height: 480px;
   position: absolute;
+  padding: 20px;
   z-index: 2;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  box-shadow: 0 4px 7px #88b8fe;
+  &__screen {
+    width: 90%;
+    height: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 4px;
+    margin: 0 auto;
+    box-shadow: 0 4px 7px #88b8fe;
+  }
+  &__scheme {
+    display: flex;
+    align-items: center;
+  }
   &__list {
     display: grid;
+    width: 90%;
+    margin: 15px auto 0;
     grid-template-columns: repeat(10, 1fr);
     grid-template-rows: repeat(10, 1fr);
-    justify-content: space-between;
     align-items: center;
+    justify-items: center;
     gap: 2px;
+    &-item {
+      height: 24px;
+      img {
+        height: 24px;
+      }
+    }
+    &-item:hover .seat__image--free {
+      transform: scale(1.2);
+    }
   }
+}
+.scheme__rows {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2px;
+  margin: 15px 0 0;
+  & .scheme__row {
+    height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+.seat__image--free {
+  cursor: pointer;
+  transition: transform 0.4s ease;
 }
 </style>
