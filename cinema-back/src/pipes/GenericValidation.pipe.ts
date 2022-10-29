1dede16e48919f7734511710f004378b76760841
Vadim Aktsenin <vadim.aktsenin@1win.pro>
2022-10-29 17:02:55 +0300

    film can be added

--- cinema-back/src/pipes/GenericValidation.pipe.ts
+++ cinema-back/src/pipes/GenericValidation.pipe.ts
@@ -11,12 +11,20 @@ import { validate } from 'class-validator';
 export class GenericValidation implements PipeTransform {
   async transform(value: any, { metatype }: ArgumentMetadata) {
     console.log(value, 'value in pipe');
-    
+    console.log(metatype, 'metatype');
+
     if (!metatype || !this.toValidate(metatype)) {
+      console.log('in falsy metatype before return');
+
       return value;
     }
     const object = plainToInstance(metatype, value);
+    console.log(object, 'object');
+
     const errors = await validate(object);
+
+    console.log(errors, 'errors after validate object');
+
     if (errors.length > 0) {
       console.log('validation failed');
       throw new BadRequestException('Validation failed');
@@ -26,7 +34,8 @@ export class GenericValidation implements PipeTransform {
 
   private toValidate(metatype: Function): boolean {
     console.log('in to validate fn');
-    
+    console.log(metatype, 'metatype in toValidate fn');
+
     const types: Function[] = [String, Boolean, Number, Array, Object];
     return !types.includes(metatype);
   }
